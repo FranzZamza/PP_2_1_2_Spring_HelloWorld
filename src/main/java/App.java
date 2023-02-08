065d6daeb6435d58f77e1a1ad28586b6a2550787
Шамсутдинов Артем Русланович <pavlovsckikh@yandex.ru>
2023-02-08 17:57:51 +0400

    init

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        //System.out.println(bean.getMessage());
+        HelloWorld beanHelloWorld =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean == beanHelloWorld);
+        System.out.println(firstCat == secondCat);
     }
+
 }
